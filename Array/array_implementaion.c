9aede7b011330234539914f5d1e37ec9ef80c8f2
S-KishoreBabu <s.kishorebabu8@gmail.com>
2024-10-13 23:24:24 +0530

    "Formatted code"

--- Array/array_implementaion.c
+++ Array/array_implementaion.c
@@ -1,8 +1,8 @@
 /*
-completed - Array Implementation and operations 
+completed - Array Implementation and operations
 
  1) Display the array values
- 2) Insert value at end 
+ 2) Insert value at end
  3) insert value at specific postion
  4) Insert value at 0th index
  5) Delete the last index value
@@ -10,167 +10,211 @@ completed - Array Implementation and operations
  7) delete a specific index
  8) delete a value of 0th index
  9) Search a specific value in array
- 
+
 */
 
-
-#include<stdio.h>
+#include <stdio.h>
 #define MAXSIZE 50
 int arr[MAXSIZE];
-int size=0;
+int size = 0;
 
-void display(){
+void display()
+{
     printf("[ ");
-    for(int i=0;i<size;i++){
-        printf("%d ",arr[i]);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
     }
     printf(" ] ");
 }
 
-int getvalue(){
+int getvalue()
+{
     int val;
     printf(" Enter value : ");
-    scanf("%d",&val);
+    scanf("%d", &val);
     return val;
 }
 
-int getpos(){
+int getpos()
+{
     int val;
     printf(" Enter position : ");
-    scanf("%d",&val);
+    scanf("%d", &val);
     return val;
 }
 
-void insert(int pos,int val){
-    if(pos==size){
-        arr[size++]=val;
+void insert(int pos, int val)
+{
+    if (pos == size)
+    {
+        arr[size++] = val;
     }
-    else{
-        for(int i=size-1;i>=pos;i--){
-            arr[i+1]=arr[i];
+    else
+    {
+        for (int i = size - 1; i >= pos; i--)
+        {
+            arr[i + 1] = arr[i];
         }
-        arr[pos]=val;
+        arr[pos] = val;
         size++;
     }
 }
 
-void deletebypos(int pos){
-    if(pos==size-1){
+void deletebypos(int pos)
+{
+    if (pos == size - 1)
+    {
         size--;
     }
-    else{
-        for(int i=pos;i<size;i++){
-            arr[i]=arr[i+1];   
+    else
+    {
+        for (int i = pos; i < size; i++)
+        {
+            arr[i] = arr[i + 1];
         }
         size--;
     }
 }
 
-int search(int val){
-    for(int i=0;i<size;i++){
-        if(arr[i]==val){
+int search(int val)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == val)
+        {
             return i;
         }
     }
     return -1;
 }
 
-int main(){
-    int option=0;
-    
+int main()
+{
+    int option = 0;
+
     printf("\n choose the option to perform operation on array :- ");
     printf("\n 1) Display \n 2) Insert value at end \n 3) insert value at specific postion \n 4) Insert value at 0th index \n 5) Delete the last index value \n 6) Delete a specific value \n 7) delete a specific index \n 8) delete a value of 0th index \n 9) Search a specific value in array \n 10) Exit ");
-    while(option!=10){
+    while (option != 10)
+    {
         printf("\n Enter option : ");
-        scanf("%d",&option);
-        switch(option){
-            case 1: 
-                if(size!=0){
-                    display();
-                }else{
-                    printf(" array is Empty");
-                }
-                break;
-            case 2: 
-                // printf("\n SZ = %d,MSZ = %d",size,MAXSIZE);
-                if(size!=MAXSIZE){
-                    int val = getvalue();
-                    insert(size,val);
-                }
-                else{
-                    printf(" array is full");
-                }
-                break;
-            case 3: 
-                if(size!=MAXSIZE){
-                    int val = getvalue();
-                    int pos = getpos();
-                    insert(pos,val);
-                }
-                else{
-                    printf(" array is full");
-                }
-                break; 
-            case 4: 
-                if(size!=MAXSIZE){
-                    int val = getvalue();
-                    insert(0,val);
-                }
-                else{
-                    printf(" array is full");
-                }
-                break;  
-            case 5:
-                if(size!=0){
-                    deletebypos(size-1);
-                }else{
-                    printf(" Array is empty");
-                } 
-                break;
-            case 6:
-                if(size!=0){
-                    int val=getvalue();
-                    int pos=search(val);
-                    if(pos!=-1){
-                        deletebypos(pos);
-                    }
-                    else{
-                        printf("val is not exist");
-                    }                            
-                }else{
-                    printf(" Array is empty");
-                }             
-                break;
-            case 7:
-                if(size!=0){
-                    int pos=getpos();
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            if (size != 0)
+            {
+                display();
+            }
+            else
+            {
+                printf(" array is Empty");
+            }
+            break;
+        case 2:
+            // printf("\n SZ = %d,MSZ = %d",size,MAXSIZE);
+            if (size != MAXSIZE)
+            {
+                int val = getvalue();
+                insert(size, val);
+            }
+            else
+            {
+                printf(" array is full");
+            }
+            break;
+        case 3:
+            if (size != MAXSIZE)
+            {
+                int val = getvalue();
+                int pos = getpos();
+                insert(pos, val);
+            }
+            else
+            {
+                printf(" array is full");
+            }
+            break;
+        case 4:
+            if (size != MAXSIZE)
+            {
+                int val = getvalue();
+                insert(0, val);
+            }
+            else
+            {
+                printf(" array is full");
+            }
+            break;
+        case 5:
+            if (size != 0)
+            {
+                deletebypos(size - 1);
+            }
+            else
+            {
+                printf(" Array is empty");
+            }
+            break;
+        case 6:
+            if (size != 0)
+            {
+                int val = getvalue();
+                int pos = search(val);
+                if (pos != -1)
+                {
                     deletebypos(pos);
-                }else{
-                    printf(" Array is empty");
-                }                 
-                break;
-            case 8:
-                if(size!=0){
-                    deletebypos(0);
-                }else{
-                    printf(" Array is empty");
-                } 
-                break; 
-            case 9:
-                if(size!=0){
-                    int value=getvalue();
-                    int pos=search(value);
-                    if(pos!=-1){
-                        printf("index = %d",pos);
-                    }
-                    else{
-                        printf("val is not exist");
-                    }                
-                }else{
-                    printf(" Array is empty");
                 }
-                break;               
-
+                else
+                {
+                    printf("val is not exist");
+                }
+            }
+            else
+            {
+                printf(" Array is empty");
+            }
+            break;
+        case 7:
+            if (size != 0)
+            {
+                int pos = getpos();
+                deletebypos(pos);
+            }
+            else
+            {
+                printf(" Array is empty");
+            }
+            break;
+        case 8:
+            if (size != 0)
+            {
+                deletebypos(0);
+            }
+            else
+            {
+                printf(" Array is empty");
+            }
+            break;
+        case 9:
+            if (size != 0)
+            {
+                int value = getvalue();
+                int pos = search(value);
+                if (pos != -1)
+                {
+                    printf("index = %d", pos);
+                }
+                else
+                {
+                    printf("val is not exist");
+                }
+            }
+            else
+            {
+                printf(" Array is empty");
+            }
+            break;
         }
     }
     return 0;
--- Array/array_pointers.c
+++ Array/array_pointers.c
@@ -1,35 +1,40 @@
-//completed - get array values function and sum of array values function using pointers
+// completed - get array values function and sum of array values function using pointers
 
-#include<stdio.h>
+#include <stdio.h>
 
-int* getarray(int);
+int *getarray(int);
 int arraysum();
 
-int main(){
-    int n,i;
+int main()
+{
+    int n, i;
     int *p;
     printf("Enter the Number of values in array : ");
-    scanf("%d",&n);
-    p=getarray(n);
-    int sum=arraysum(p,n);
-    printf("\n Sum of array values : %d",sum);
+    scanf("%d", &n);
+    p = getarray(n);
+    int sum = arraysum(p, n);
+    printf("\n Sum of array values : %d", sum);
     return 0;
 }
 
-int arraysum(int *x,int n){
-    int sum=0;
-    for(int i=0;i<n;i++){
-        sum+=*(x+i);
+int arraysum(int *x, int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += *(x + i);
     }
     return sum;
 }
 
-int* getarray(int n){
-    static int arr[100],i;
+int *getarray(int n)
+{
+    static int arr[100], i;
     printf("\n\nEnter the values : ");
-    for(i=0;i<n;i++){
-        printf("\n %d : ",i+1);
-        scanf("%d",&arr[i]);
+    for (i = 0; i < n; i++)
+    {
+        printf("\n %d : ", i + 1);
+        scanf("%d", &arr[i]);
     }
     return arr;
 }
