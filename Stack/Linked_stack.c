b446b31ccd37c01feb070b484ed124c083bd45fe
S-KishoreBabu <s.kishorebabu8@gmail.com>
2024-11-30 22:03:15 +0530

    new file uploaded

---
+++ Stack/Linked_stack.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <malloc.h>
+
+struct lnode
+{
+    int data;
+    struct lnode *next;
+};
+int n = -1;
+typedef struct lnode node;
+
+node *top = NULL;
+
+node *createnode(int val)
+{
+    node *newnode = (node *)malloc(sizeof(node));
+    newnode->data = val;
+    newnode->next = NULL;
+    return newnode;
+}
+
+void push(int val)
+{
+    node *newnode = createnode(val);
+    if (top != NULL)
+    {
+        newnode->next = top;
+    }
+    top = newnode;
+    n++;
+}
+
+int pop()
+{
+    if (top != NULL)
+    {
+        node *temp = top;
+        int val = temp->data;
+        top = top->next;
+        free(temp);
+        return val;
+        n--;
+    }
+    printf("\n Stack is empty\n");
+    return -1;
+}
+void display()
+{
+    if (top != NULL)
+    {
+        node *temp = top;
+        printf("\n|'''|");
+        while (temp != NULL)
+        {
+            printf("\n| %d |", temp->data);
+            printf("\n|___|");
+            temp = temp->next;
+        }
+    }
+    else
+    {
+        printf("\n Stack is empty\n");
+    }
+}
+
+int peek()
+{
+    if (top != NULL)
+    {
+        int val = top->data;
+        return val;
+    }
+    printf("stack is empty");
+}
+
+void posofStack()
+{
+    printf("\n Position of top is %d ", n);
+}
+
+int main()
+{
+    int n, x;
+    printf("\n Choose the option :-  \n 1) push()  2) pop()  3) peek()  4) display  5) position of top 6) Exit");
+    do
+    {
+        printf("\n Enter option : ");
+        scanf("%d", &n);
+        switch (n)
+        {
+        case 1:
+            printf("Enter  the value to push in stack :- ");
+            scanf("%d", &x);
+            push(x);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            x = peek();
+            if (top != NULL)
+            {
+                printf("Top value = %d", x);
+            }
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            posofStack();
+            break;
+        default:
+            printf("\n You have entered wrong option");
+        }
+    } while (n != 6);
+    return 0;
+}
---
+++ Stack/stack_application/List_reversal.c
@@ -0,0 +1,49 @@
+// Reversing a list
+
+#include<stdio.h>
+#include<conio.h>
+
+#define MAXSIZE 10
+int stack[MAXSIZE];
+
+int top=-1;
+
+void push(int val)
+{
+    if (top != MAXSIZE - 1)
+    {
+        stack[++top] = val;
+    }
+    else
+    {
+        printf("\n stack is full");
+    }
+}
+
+int pop()
+{
+    if (top!=-1)
+    {
+        int temp = stack[top];
+        top--;
+        return temp;
+    }
+    printf("\n stack is empty");
+    return -1;
+}
+
+int main(){
+    int arr[]={1,2,3,4,5,6,7};
+    for(int i=0;i<(sizeof(arr)/sizeof(arr[0]));i++){
+        printf("%d",arr[i]);
+        push(arr[i]);
+    }
+    for(int i=0;i<(sizeof(arr)/sizeof(arr[0]));i++){
+        arr[i]=pop();
+    }
+    printf("\n Reversed List : ");
+    for(int i=0;i<(sizeof(arr)/sizeof(arr[0]));i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
