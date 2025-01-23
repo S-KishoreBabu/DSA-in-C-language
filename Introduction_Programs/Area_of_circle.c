60e0f2d51a6019038c11199e56e6f8111365302e
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-01-23 22:06:22 +0530

    -

---
+++ Introduction_Programs/Area_of_circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main(){
+    float r=7;
+    double area;
+    system("cls");
+    printf("\n Enter Radius : ");
+    scanf("%f",&r);
+    area = (r*r)*3.14;
+    printf("\n Area = %.2lf",area);
+    return 0;
+}
