0571dc4ca8bdfa6bc57a16e97d80b4c746002c23
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-01-24 19:26:19 +0530

    -

---
+++ Introduction_Programs/int_to_float.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//#include<stdlib.h>
+
+int main(){
+    int i_num;
+    float f_num;
+    //system("cls");
+    printf("\nEnter an integer Number : ");
+    scanf("%d",&i_num);
+    f_num=(float)i_num;
+    printf("\nThe floating point variant of %d is = %f",i_num,f_num);
+    return 0;
+}
