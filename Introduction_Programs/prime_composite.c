945c48e4edbcb7f10685b4144c45019ba9ad8762
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-01-25 12:53:37 +0530

    Create and upload program to determine prime or composite number

---
+++ Introduction_Programs/prime_composite.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int flag = 0,i,num;
+    system("cls");
+    printf("\nEnter an number : ");
+    scanf("%d",&num);
+    for(i=2;i<num/2;i++){
+        if(num%i==0){
+            flag=1;
+            break;
+        }
+    }
+    if(flag == 1){
+        printf("\n %d is composite Number",num);
+    }else{
+        printf("\n %d is Prime Number",num);
+    }
+    return 0;
+}
