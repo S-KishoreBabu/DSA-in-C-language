8735e9209d5f3042812e4e31d7f002f4958c09a0
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-01-25 12:31:07 +0530

    Add C program to calculate average of first n numbers

---
+++ Introduction_Programs/avg_first_n.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int n,sum=0,i=0;
+    float result;
+    system("cls");
+    printf("Enter the value of n : ");
+    scanf("%d",&n);
+    do{
+        sum+=i;
+        i++;
+    }while(i<=n);
+    result = (float)sum/n;
+    printf("\n The Average of first %d numbers = %.2lf ",n,result);
+    return 0;
+}
