0ed5fdb58b92e0d50e73c46f24ee8cbacaf6ffaf
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-01-25 12:10:19 +0530

    created & uploaded

---
+++ Introduction_Programs/sum_m_to_n.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int m,n,i,sum=0;
+    system("cls");
+    printf("Enter a starting Number : ");
+    scanf("%d",&m);
+    printf("Enter the last Number :");
+    scanf("%d",&n);
+    i=m;
+    while(i<=n){
+        sum+=i;
+        i++;
+    }
+    printf("\n The sum of the numbers from %d to %d = %d",m,n,sum);
+    return 0;
+}
