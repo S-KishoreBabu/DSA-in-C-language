4d8605f71603b9ae07bacca0d92a6e2e46286573
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-01-25 11:48:56 +0530

    created & uploaded

---
+++ Introduction_Programs/vowel_check.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    char ch;
+    system("cls");
+    printf("Enter an character : ");
+    scanf("%c",&ch);
+    switch (ch)
+    {
+    case 'A':
+    case 'a':
+        printf("\n %c is Vowel",ch);
+        break;
+    case 'E':
+    case 'e':
+        printf("\n %c is Vowel",ch);
+        break;
+    case 'I':
+    case 'i':
+        printf("\n %c is Vowel",ch);
+        break;
+    case 'O':
+    case 'o':
+        printf("\n %c is Vowel",ch);
+        break;
+    case 'U':
+    case 'u':
+        printf("\n %c is Vowel",ch);
+        break;    
+    default:
+        printf("\n %c is Not a vowel",ch);
+        break;
+    }
+    return 0;
+}
