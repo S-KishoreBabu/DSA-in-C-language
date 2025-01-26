3e0a04879b4e22a9d9250cba4ecdfe4133167739
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-01-26 09:42:12 +0530

    Add program to calculate mean using arrays

---
+++ Array/mean_calculator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int arr[20],i,n,sum=0;
+    float mean;
+    system("cls");
+    printf("Enter the Number of elements in the array : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("\n arr[%d] = ",i);
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    mean=(float)sum/n;
+    printf("\n The mean of the array elements = %.2lf",mean);
+    
+    return 0;
+}
