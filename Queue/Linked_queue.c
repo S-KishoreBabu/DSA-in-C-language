119eaadf183cb8b793367539471215bb071cc9ad
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-02-02 18:29:25 +0530

    -

--- Queue/Linked_queue.c
+++ Queue/Linked_queue.c
@@ -1,3 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct queue{
+    int data;
+    struct queue *next;
+};
+
+typedef struct queue q;
+
+q* createqnode(int val){
+    q* node=(q*)malloc(sizeof(q));
+    node->data=val;
+    node->next=NULL;
+    return node;
+}
+
+
