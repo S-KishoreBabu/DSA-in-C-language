1ef9cd453adcd774589db7904f068cf6958c0481
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-02-02 19:09:45 +0530

    `Added tail pointer and implemented dequeue function to Linked Queue implementation`

--- Queue/Linked_queue.c
+++ Queue/Linked_queue.c
@@ -4,26 +4,42 @@
 struct queue{
     int data;
     struct queue *next;
+    struct queue *prev;
 };
 
 typedef struct queue q;
 q *head = NULL;
+q *tail = NULL;
 
 q* createqnode(int val){
     q* node=(q*)malloc(sizeof(q));
     node->data=val;
     node->next=NULL;
+    node->prev=NULL;
     return node;
 }
 
 void enqueue(int val){
     if(head==NULL){
         head=createqnode(val);
+        tail=head;
         return;
     }
     q* newnode=createqnode(val);
     newnode->next=head;
+    head->prev=newnode;
     head=newnode;
+}
+
+q* dequeue(){
+    if(head!=NULL){
+        q* temp=tail;
+        tail=tail->prev;
+        tail->next=NULL;
+        temp->prev=NULL;
+        return temp;
+    }
+    printf("\n queue is Empty");
 }
 
 
