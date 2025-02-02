3b5d0f09308f7dc27849e4addf1f1da45c7de886
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-02-02 20:37:02 +0530

    modified:   Queue/Linked_queue.c

--- Queue/Linked_queue.c
+++ Queue/Linked_queue.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
 struct queue{
     int data;
@@ -32,15 +33,11 @@ void enqueue(int val){
 }
 
 q* dequeue(){
-    if(head!=NULL){
-        q* temp=tail;
-        tail=tail->prev;
-        tail->next=NULL;
-        temp->prev=NULL;
-        return temp;
-    }
-    printf("\n queue is Empty");
-    return;
+    q* temp=tail;
+    tail=tail->prev;
+    tail->next=NULL;
+    temp->prev=NULL;
+    return temp;
 }
 
 
@@ -58,6 +55,54 @@ void display(){
     printf("\n Queue is empty");
 }
 
+
+int queuePeek(){
+    if(head!=NULL){
+        return tail->data;
+    }
+    printf("\n Queue is empty");
+}
+
+bool isqueueempty(){
+    return head==NULL;
+}
+
+
+int main(){
+    int n,val;
+    system("cls");
+    printf("\n 1) Enqueue \n 2) Dequeue \n 3) Peek \n 4 display the queue \n 5) Exit \n Select the Operation to Perform : ");
+    scanf("%d",&n);
+    while(n!=5){
+        switch(n){
+            case 1:
+                printf("\n Enter the value to enqueue : ");
+                scanf("%d",&val);
+                break;
+            case 2:
+                if(head!=NULL){
+                    q* node = dequeue();
+                    printf("\n Dequeued node value is %d ",node->data);
+                }
+                else{
+                    printf("\n Queue is empty");
+                }
+                break;
+            case 3:
+                printf("\n peek value = %d ",queuePeek());
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                exit(0);
+                break;
+            default:
+                printf("\n You Entered wrong input ");
+        }
+    }
+    return 0;
+}
 
 
 
