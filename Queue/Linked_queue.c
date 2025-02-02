60705dafae3c98b1628a0e6451ec35fcbd8a4107
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-02-02 18:43:12 +0530

    enqueue function created

--- Queue/Linked_queue.c
+++ Queue/Linked_queue.c
@@ -7,12 +7,23 @@ struct queue{
 };
 
 typedef struct queue q;
+q *head = NULL;
 
 q* createqnode(int val){
     q* node=(q*)malloc(sizeof(q));
     node->data=val;
     node->next=NULL;
     return node;
+}
+
+void enqueue(int val){
+    if(head==NULL){
+        head=createqnode(val);
+        return;
+    }
+    q* newnode=createqnode(val);
+    newnode->next=head;
+    head=newnode;
 }
 
 
