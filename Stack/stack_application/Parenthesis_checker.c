6b2daf9273a13c4d7710dde63ef7fad9edd4bcbe
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-02-08 23:34:01 +0530

    `Added push function to stack implementation in Parenthesis_checker.c`

--- Stack/stack_application/Parenthesis_checker.c
+++ Stack/stack_application/Parenthesis_checker.c
@@ -8,7 +8,14 @@
 char stack[max];
 int top=-1;
 
-
+void push(char ch){
+    if(top!=max-1){
+        stack[++top]=ch;
+    }
+    else{
+        printf("Stack is full\n");
+    }
+}
 
 int main(){
     system("cls");
