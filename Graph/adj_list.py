f2e52be8afc7e4a9f37a58ce66a034cca7738ed9
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-10-02 20:01:14 +0530

    adj_list of python file uploaded

---
+++ Graph/adj_list.py
@@ -0,0 +1,21 @@
+class Graph:
+    def __init__(self):
+        self.list = {}
+
+    def addEdge(self,i,j):
+        self.list.setdefault(i,[]).append(j)
+        self.list.setdefault(j,[]).append(i)
+
+    def display(self):
+        for vertex,edges in self.list.items():
+            print(f"[{vertex}]-> {edges}")
+
+graph = Graph()
+graph.display()
+graph.addEdge(0,1)
+graph.addEdge(0,4)
+graph.addEdge(1,2)
+graph.addEdge(1,4)
+graph.addEdge(2,3)
+graph.addEdge(3,4)
+graph.display()
