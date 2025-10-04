20cc0ae7c5319d709b9dd68fb818870d2d81860b
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-10-04 22:32:32 +0530

    BFS of c lang completed

---
+++ Graph/BFS.c
@@ -0,0 +1,114 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct gnode{
+    int val;
+    char alpha;
+    struct gnode*next;
+};
+
+typedef struct gnode node;
+
+struct graph{
+    int verticesNum;
+    char *array;
+    node**list;
+};
+typedef struct graph Graph;
+
+node* createNode(int value,char alphabet){
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->val=value;
+    newNode->alpha = alphabet;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Graph* createGraph(int NumberOfVertices){
+    Graph* newGraph = (Graph*)malloc(sizeof(Graph));
+    char input;
+    newGraph->verticesNum = NumberOfVertices;
+    newGraph->list = (node**)malloc(NumberOfVertices*sizeof(node*));
+    newGraph->array = (char*)malloc(NumberOfVertices*sizeof(char));
+    for(int i = 0;i<NumberOfVertices;i++){
+        newGraph->list[i] = NULL;
+        printf("\n Enter character for %dth vertex : ",i);
+        scanf(" %c",&input);
+        newGraph->array[i] = input;
+    }
+    return newGraph;
+}
+
+void addEdge(Graph *graph,int i,int j,char a,char b){
+    node* newNode = createNode(j,b);
+    newNode->next = graph->list[i];
+    graph->list[i] = newNode;
+
+    newNode = createNode(i,a);
+    newNode->next = graph->list[j];
+    graph->list[j] = newNode;
+}
+
+void display(Graph *graph) {
+    for (int i = 0; i < graph->verticesNum; i++) {
+        node* temp = graph->list[i];
+        printf("%c ->", graph->array[i]);
+        while (temp) {
+            printf(" %c", temp->alpha);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+
+void BFS(Graph *graph) {
+    int size = graph->verticesNum;
+    int queue[size], visited[size];// hash
+    int front = 0, rear = 0;
+
+    for (int i = 0; i < size; i++)
+        visited[i] = 0;
+
+    queue[rear++] = 0;
+    visited[0] = 1;
+
+    printf("\nBFS = ");
+    while (front < rear) {
+        int current = queue[front++];
+        printf("%c ", graph->array[current]);
+
+        node* temp = graph->list[current];
+        while (temp) {
+            if (!visited[temp->val]) {
+                queue[rear++] = temp->val;
+                visited[temp->val] = 1;
+            }
+            temp = temp->next;
+        }
+    }
+}
+
+int main(){
+    int v;
+    printf("Enter no. of vertices : ");
+    scanf("%d",&v);
+    Graph* graph = createGraph(v);
+    display(graph);
+    printf("\n\n");
+    addEdge(graph, 0, 1, graph->array[0], graph->array[1]);
+    addEdge(graph, 0, 2, graph->array[0], graph->array[2]);
+    addEdge(graph, 0, 4, graph->array[0], graph->array[4]);
+    addEdge(graph, 1, 4, graph->array[1], graph->array[4]);
+    addEdge(graph, 2, 3, graph->array[2], graph->array[3]);
+    addEdge(graph, 2, 5, graph->array[2], graph->array[5]);
+    addEdge(graph, 3, 4, graph->array[3], graph->array[4]);
+    addEdge(graph, 4, 6, graph->array[4], graph->array[6]);
+    addEdge(graph, 4, 7, graph->array[4], graph->array[7]);
+    addEdge(graph, 5, 6, graph->array[5], graph->array[6]);
+    addEdge(graph, 6, 7, graph->array[6], graph->array[7]);
+    display(graph);
+    BFS(graph);
+    return 0;
+}
+
