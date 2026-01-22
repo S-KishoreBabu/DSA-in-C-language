2e7a01ed78e98616cde2fcab4981252b5075d17d
S-KishoreBabu <s.kishorebabu8@gmail.com>
2026-01-22 10:04:13 +0530

    updated Bingo

---
+++ nodeExpress/index.js
@@ -0,0 +1,65 @@
+const express=require('express')
+const app=express()
+const bodyParser=require('body-parser')
+app.use(bodyParser.json())
+
+const data=[
+      {
+    id: 1,
+    name: "Laptop",
+    price: 60000,
+    quantity: 1
+  },
+  {
+    id: 2,
+    name: "Mobile Phone",
+    price: 25000,
+    quantity: 2
+  },
+  {
+    id: 3,
+    name: "Headphones",
+    price: 3000,
+    quantity: 1
+  },
+  {
+    id: 4,
+    name: "Keyboard",
+    price: 1500,
+    quantity: 1
+  },
+  {
+    id: 5,
+    name: "Mouse",
+    price: 800,
+    quantity: 3
+  }
+
+
+]
+app.get('/',(req,res)=>{res.json(data)})
+
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    data.push(req.body)
+    res.json(data)
+})
+app.patch("/:index",(req,res)=>{
+    const index = req.params.index
+    data[index] = {
+        ...data[index],
+        ...req.body
+    }
+    res.json(data)
+})
+
+app.delete("/:index",(req,res)=>{
+    data.splice(req.params.index,1)
+    res.json(data)
+})
+app.put("/:index",(req,res)=>{
+    data[req.params.index]=req.body
+    res.json(data)
+})
+
+app.listen(3000,()=>{console.log('Server running on port 3000')})
